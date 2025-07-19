d48f4f6bd289f50b2cb83dea8642497c23165b76
902408 <hamidreza.abedini@gmail.com>
2025-07-19 14:28:33 +0330

    *

---
+++ Scripts/Char Type Len.sql
@@ -0,0 +1,77 @@
+﻿
+WITH words (lang, word) AS (
+	 SELECT 'Persian',
+			'درود'
+	 FROM DUAL d
+	   UNION ALL
+	 SELECT 'English',
+			'Hello'
+	 FROM DUAL d
+	   UNION ALL
+	 SELECT 'China',
+			'问候'
+	 FROM DUAL d
+	   UNION ALL
+	 SELECT 'Emoji',
+			'😊'
+	 FROM DUAL d
+   ),
+tp_char AS (
+	 SELECT lang,
+			--CAST(word AS nvarCHAR2(10)) AS word, -- ORA-12704: character set mismatch	  
+			word,
+			'char' Type,
+			LENGTH(CAST(word AS CHAR(10))) len_char,
+			LENGTHB(CAST(word AS CHAR(10))) len_byte,
+			LENGTHB(CAST(word AS CHAR(10))) / LENGTH(CAST(word AS CHAR(10))) Coefficient
+
+	 FROM words
+   ),
+tp_varchar AS (
+	 SELECT lang,
+			word,
+			'varchar' Type,
+			LENGTH(CAST(word AS VARCHAR2(10))) len_char,
+			LENGTHB(CAST(word AS VARCHAR2(10))) len_byte,
+			LENGTHB(CAST(word AS VARCHAR2(10))) / LENGTH(CAST(word AS VARCHAR(10))) Coefficient
+
+	 FROM words
+   ),
+tp_nvarchar AS (
+	 SELECT lang,
+			word,
+			'nvarchar' Type,
+			LENGTH(CAST(word AS NVARCHAR2(10))) len_char,
+			LENGTHB(CAST(word AS NVARCHAR2(10))) len_byte,
+			LENGTHB(CAST(word AS NVARCHAR2(10))) / LENGTH(CAST(word AS NVARCHAR2(10))) Coefficient
+
+	 FROM words
+   ),
+final AS (
+
+	 SELECT *
+	 FROM tp_char
+	   UNION
+	 SELECT *
+	 FROM tp_varchar
+	   UNION
+	 SELECT *
+	 FROM tp_nvarchar
+   )
+SELECT (
+		   SELECT VALUE
+		   FROM NLS_DATABASE_PARAMETERS
+		   WHERE PARAMETER = 'NLS_CHARACTERSET'
+	   ) NLS_CHARACTERSET,
+	   lang,
+	   word,
+	   TYPE,
+	   len_char,
+	   len_byte,
+	   round(Coefficient,2) AS Coefficient
+
+FROM final
+--ORDER BY word, TYPE
+ORDER BY  TYPE
+
+;
---
+++ Scripts/NLS_VALID_VALUES.sql
@@ -0,0 +1,29 @@
+﻿SELECT PARAMETER,
+	   COUNT(*) total,
+	   SUM(DECODE(ISDEPRECATED, 'TRUE', 1, 0)) is_deprecated,
+	   SUM(DECODE(ISDEPRECATED, 'FALSE', 1, 0)) is_not_deprecated
+FROM V$NLS_VALID_VALUES
+GROUP BY PARAMETER;
+
+-------------------------------------------------- LANGUAGE
+SELECT *
+FROM V$NLS_VALID_VALUES
+WHERE PARAMETER = 'LANGUAGE';
+
+-------------------------------------------------- TERRITORY
+SELECT *
+FROM V$NLS_VALID_VALUES
+WHERE PARAMETER = 'TERRITORY';
+
+-------------------------------------------------- CHARACTERSET
+SELECT *
+FROM V$NLS_VALID_VALUES
+WHERE PARAMETER = 'CHARACTERSET';
+
+-------------------------------------------------- SORT
+SELECT *
+FROM V$NLS_VALID_VALUES
+WHERE PARAMETER = 'SORT';
+
+
+
